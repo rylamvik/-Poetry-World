7709a25fd6039aa6e6ea7b7c5c1e8ea0ac7d4e1b
rylamvik <rylamvik@gmail.com>
2017-02-13 21:29:25 -0800

    inital commit

---
+++ index.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <script src="https://aframe.io/releases/0.4.0/aframe.min.js"></script>
+  </head>
+  <body>
+    
+	<a-scene>
+ 	 <a-box src="https://i.imgur.com/mYmmbrp.jpg" position="0 2 -5" rotation="0 90 0" scale="1 1 1">
+ 	 	<a-sphere color="blue" position="0 0 3"></a-sphere>
+ 	 	<a-sphere color="green" position="0 0 -3"></a-sphere>
+ 	 	<a-sphere color="yellow" position="0 3 0"></a-sphere>
+ 	 	<a-sphere color="pink" position="0 -3 0"></a-sphere>
+
+ 	 	<a-sky color="#222"></a-sky>
+ 	 </a-box>
+	</a-scene>
+  
+  <body>
+</html>
